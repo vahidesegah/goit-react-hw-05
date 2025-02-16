05f6128bc67a87d494e88cf960d2733e9ae9dd73
Vahide Segah Üney <vahidesegah@gmail.com>
2025-02-16 14:35:39 +0300

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "../assets/HomePage";
+import "../assets/MovieCast";
+import "../assets/MovieDetailsPage";
+import "../assets/MovieList";
+import "../assets/MovieReviews";
+import "../assets/MoviesPage";
+import "../assets/NotFoundPage";
+import "../assets/api";
+import "../assets/index";
 import "./App.css";
 
 function App() {
