5d61a1214118e3485590713f7d4c9d44b7da48f6
Vahide Segah Üney <vahidesegah@gmail.com>
2025-02-16 14:21:25 +0300

    nosolve

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/"></BrowserRouter>
     <App />
   </StrictMode>
 );
